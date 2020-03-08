98448da63ef730d39d3ec187e85f0c35be512531
Joseoliva762Win <joseolivacr@unimagdalena.edu.co>
2020-03-08 13:29:39 -0500

    Fixed Home direction

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/#">
+          <a href="/Cientifico-SPA/#">
             100tifi.co
           </a>
         </h1>
