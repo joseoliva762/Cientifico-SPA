75245e6e111d596b914c54f8ba753f158f3d2065
Joseoliva762Win <joseolivacr@unimagdalena.edu.co>
2020-03-08 11:48:36 -0500

    Proyecto v1.0

---
+++ src/templates/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+    const view = `
+      <div class="Footer-main">
+        <div class="Header-logo">
+          <h1>
+            <a href="/">
+              100tifi.co
+            </a>
+          </h1>
+        </div>
+        <div class="Header-nav">
+          <a href="#/about/">
+            About
+          </a>
+        </div>
+      </div>
+    `;
+    return view;
+  };
+  
+  export default Header;
